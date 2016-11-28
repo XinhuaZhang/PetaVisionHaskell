7d75acdf647b4318735d3f6ab0ce73b3787b2c12
Xinhua Zhang <xinhua@unm.edu>
2016-11-28 11:21:06 -0700

    made the program to accept a set of reconfile name, then ploting them one by one and combining them

--- Application/PlotRecon/Main.hs
+++ Application/PlotRecon/Main.hs
@@ -4,24 +4,48 @@ import           Codec.Picture
 import           Data.Array.Repa        as R
 import           Data.Conduit
 import           Data.Conduit.List      as CL
+import           Data.List              as L
+import           Data.Maybe             as Maybe
 import           PetaVision.Data.Weight
 import           PetaVision.PVPFile.IO
 import           Prelude                as P
 import           System.Directory
 import           System.Environment
-import Data.List as L
 
 main = do
-  (reconFile:_) <- getArgs
-  recon <- pvpFileSource reconFile $$ CL.head
+  reconFiles <- getArgs
+  recons <- P.mapM (\reconFile -> pvpFileSource reconFile $$ CL.head) reconFiles
   dir <- getCurrentDirectory
   createDirectoryIfMissing True (dir P.++ "/Recon")
-  case recon of
-    Nothing -> error "Read recon error"
-    Just (PVP_OUTPUT_NONSPIKING_ACT (PVPDimension nx ny nf) y) -> do
-      let arr = R.fromUnboxed (Z :. ny :. nx :. nf) y
-          normalizedWP =
-            normalizeWeightPatch (P.fromIntegral (maxBound :: Pixel8)) arr
-          ind = L.findIndices (=='/') reconFile
-          (_,fileName) = L.splitAt (L.last ind) reconFile
-      plotWeightPatch (dir P.++ "/Recon/" P.++ fileName P.++ ".png") normalizedWP
+  if P.any isNothg recons
+    then error "Read recon error"
+    else let arrs =
+               L.map
+                 (\(PVP_OUTPUT_NONSPIKING_ACT (PVPDimension nx ny nf) y) ->
+                     R.fromUnboxed (Z :. ny :. nx :. nf) y) .
+               Maybe.catMaybes $
+               recons
+             normalizedArrs =
+               L.map
+                 (normalizeWeightPatch (P.fromIntegral (maxBound :: Pixel8)))
+                 arrs
+             fileNames =
+               L.map
+                 (\reconFile ->
+                     snd $
+                     L.splitAt (L.last (L.findIndices (== '/') reconFile)) reconFile)
+                 reconFiles
+             combinedArr =
+               normalizeWeightPatch (P.fromIntegral (maxBound :: Pixel8)) .
+               L.foldl1' (+^) $
+               arrs
+         in L.zipWithM_
+              (\fileName normalizedArr ->
+                  plotWeightPatch
+                    (dir P.++ "/Recon/" P.++ fileName P.++ ".png")
+                    normalizedArr)
+              fileNames
+              normalizedArrs
+              plotWeightPatch
+              (dir P.++ "/Recon/recon.png")
+              combinedArr
