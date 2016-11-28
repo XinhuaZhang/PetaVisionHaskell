6889d50d7ec82689fc19586a02b22b325e2c0591
Xinhua Zhang <xinhua@unm.edu>
2016-11-28 10:47:46 -0700

    Added PlotRecon module

---
+++ Application/PlotRecon/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import           Codec.Picture
+import           Data.Array.Repa        as R
+import           Data.Conduit
+import           Data.Conduit.List      as CL
+import           PetaVision.Data.Weight
+import           PetaVision.PVPFile.IO
+import           Prelude                as P
+import           System.Directory
+import           System.Environment
+
+main = do
+  (reconFile:folderName:_) <- getArgs
+  recon <- pvpFileSource reconFile $$ CL.head
+  dir <- getCurrentDirectory
+  removePathForcibly (dir P.++ "/Recon/" P.++ folderName)
+  createDirectoryIfMissing True (dir P.++ "/Recon/" P.++ folderName)
+  case recon of
+    Nothing -> error "Read recon error"
+    Just (PVP_OUTPUT_NONSPIKING_ACT (PVPDimension nx ny nf) y) -> do
+      let arr = R.fromUnboxed (Z :. ny :. nx :. nf) y
+          normalizedWP =
+            normalizeWeightPatch (P.fromIntegral (maxBound :: Pixel8)) arr
+      plotWeightPatch (dir P.++ "/Recon/" P.++ folderName) normalizedWP
