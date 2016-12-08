6cbe1d6b51b4cb3382767fd92b48e890b52e82e5
xinhuazhang <fantasyfrog@gmail.com>
2016-12-07 18:53:56 -0700

    Added print time function for monitoring

--- Application/GMM/ConvertPVPGMM.hs
+++ Application/GMM/ConvertPVPGMM.hs
@@ -6,11 +6,12 @@ import           Data.Conduit
 import           Data.List                            as L
 import           PetaVision.PVPFile.IO
 import           PetaVision.Utility.Parallel
+import           PetaVision.Utility.Time
 import           System.Environment
 import           System.IO
 
 main = do
-  let parallelParams = ParallelParams 8 800
+  let parallelParams = ParallelParams 4 10000
   filePathList <- getArgs
   withBinaryFile
     (L.head filePathList)
@@ -20,13 +21,14 @@ main = do
           (\h1 filePath -> do
              header <- readPVPHeader filePath
              M.foldM
-               (\handle featureInd ->
-                   pvpFileSource filePath $$
-                   unpooledSparse2NonsparseConduit parallelParams featureInd =$=
-                   hFeatureSink
-                     handle
-                     (nBands header)
-                     (L.product . L.map (\f -> f header) $ [nx, ny]))
+               (\handle featureInd -> do
+                  printCurrentTime
+                  pvpFileSource filePath $$
+                    unpooledSparse2NonsparseConduit parallelParams featureInd =$=
+                    hFeatureSink
+                      handle
+                      (nBands header)
+                      (L.product . L.map (\f -> f header) $ [nx, ny]))
                h1
                [0 .. nf header - 1])
           h $
