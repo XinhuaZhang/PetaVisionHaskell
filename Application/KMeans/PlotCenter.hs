633780df9adbb143e443713d2a64195255cd09bb
Xinhua Zhang <xinhua@unm.edu>
2017-02-09 17:50:01 -0700

    Added convolution module; It is an unfinished version

--- Application/KMeans/PlotCenter.hs
+++ Application/KMeans/PlotCenter.hs
@@ -12,26 +12,44 @@ import           Data.Vector.Unboxed           as VU
 import           PetaVision.Data.Weight
 import           PetaVision.PVPFile.IO
 import           System.Environment
+import Data.Array.Unboxed as AU
 
 main = do
   (kmeansFile:weightFile:_) <- getArgs
   kmeansModel <- readKMeansModel kmeansFile
   weight <- runResourceT $ pvpFileSource weightFile $$ CL.head
+  let bound = Z :. 3 :. 3 :. 48 :: DIM3
+      isConvolution =
+        if (L.last . listOfShape $ bound) == 1
+          then False
+          else True
   case weight of
     Nothing -> error "Read weight error"
     Just (PVP_OUTPUT_KERNEL w) -> do
       let ys =
-            L.map
-              (\zs ->
-                  let vec = fromListUnboxed (Z :. (L.length zs)) zs
-                  in sumS $
-                     traverse2
-                       w
-                       vec
-                       const
-                       (\fw fmu idx@(Z :. _j :. _i :. _k :. n) ->
-                           fw idx * fmu (Z :. n)))
-              kmeansModel
+            if isConvolution
+              then let (Z :. )
+                       weight' = 
+                   in L.map
+                        (\zs ->
+                            let (Z :. ny' :. nx' :. nf') = bound
+                                arr =
+                                  listArray
+                                    ((0, 0, 0), (ny' - 1, nx' - 1, nf' - 1))
+                                    zs
+                            in undefined)
+                        kmeansModel
+              else L.map
+                     (\zs ->
+                         let vec = fromListUnboxed (Z :. (L.length zs)) zs
+                         in sumS $
+                            traverse2
+                              w
+                              vec
+                              const
+                              (\fw fmu idx@(Z :. _j :. _i :. _k :. n) ->
+                                  fw idx * fmu (Z :. n)))
+                     kmeansModel
       savePngImage
         "dictionary.png"
         (getGridImage $ normalizeWeight (fromIntegral (maxBound :: Pixel8)) w)
