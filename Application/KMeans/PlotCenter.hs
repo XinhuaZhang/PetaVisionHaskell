a8f4076d1a5c5a50d504ad566e2b831db26f3032
Xinhua Zhang <xinhua@unm.edu>
2017-02-10 11:13:23 -0700

    Added convolutional way of ploting weight

--- Application/KMeans/PlotCenter.hs
+++ Application/KMeans/PlotCenter.hs
@@ -1,44 +1,89 @@
+{-# LANGUAGE FlexibleContexts #-}
 import           Application.KMeans.ArgsParser as Parser
+import           PetaVision.Utility.Array
 import           Application.KMeans.Conduit
 import           Application.PlotWeight.Grid
 import           Codec.Picture
 import           Control.Monad                 as M
 import           Control.Monad.Trans.Resource
 import           Data.Array.Repa               as R
+import           Data.Array.Unboxed            as AU
 import           Data.Conduit
 import           Data.Conduit.List             as CL
 import           Data.List                     as L
 import           Data.Vector.Unboxed           as VU
+import           PetaVision.Data.Convolution
 import           PetaVision.Data.Weight
 import           PetaVision.PVPFile.IO
 import           System.Environment
-import Data.Array.Unboxed as AU
+
+{-# INLINE uArray2RepaArray3 #-}
+
+uArray2RepaArray3 :: UArray (Int, Int, Int) Double -> R.Array U DIM3 Double
+uArray2RepaArray3 arr =
+  let ((lb1, lb2, lb3), (ub1, ub2, ub3)) = bounds arr
+  in fromListUnboxed
+       (Z :. (ub1 - lb1 + 1) :. (ub2 - lb2 + 1) :. (ub3 - lb3 + 1)) .
+     elems $
+     arr
+     
+{-# INLINE repaArray2UArray2 #-}  
+
+repaArray2UArray2
+  :: (R.Source s Double)
+  => R.Array s DIM2 Double -> UArray (Int, Int) Double
+repaArray2UArray2 arr =
+  let (Z :. ny' :. nx') = extent arr
+  in listArray ((0, 0), (ny' - 1, nx' - 1)) . R.toList $ arr
 
 main = do
   (kmeansFile:weightFile:_) <- getArgs
   kmeansModel <- readKMeansModel kmeansFile
   weight <- runResourceT $ pvpFileSource weightFile $$ CL.head
-  let bound = Z :. 3 :. 3 :. 48 :: DIM3
+  let (actNy, actNx, actNf) = (3, 3, 48) :: (Int, Int, Int)
+      xs =
+        L.map
+          (\x' ->
+              let arr' = fromListUnboxed (Z :. actNy :. actNx :. actNf) x'
+              in L.map
+                   (\i -> R.slice arr' (Z :. All :. All :. i))
+                   [0 .. actNf - 1])
+          kmeansModel
       isConvolution =
-        if (L.last . listOfShape $ bound) == 1
+        if actNy == 1
           then False
           else True
+      stride = 1
   case weight of
     Nothing -> error "Read weight error"
     Just (PVP_OUTPUT_KERNEL w) -> do
       let ys =
             if isConvolution
-              then let (Z :. )
-                       weight' = 
-                   in L.map
-                        (\zs ->
-                            let (Z :. ny' :. nx' :. nf') = bound
-                                arr =
-                                  listArray
-                                    ((0, 0, 0), (ny' - 1, nx' - 1, nf' - 1))
-                                    zs
-                            in undefined)
-                        kmeansModel
+              then let (Z :. ny' :. nx' :. nf' :. numDict) = extent w
+                       weightList =
+                         L.map
+                           (\i -> R.slice w (Z :. All :. All :. All :. i))
+                           [0 .. numDict - 1]
+                   in L.zipWith
+                        (\x weight' ->
+                            sumS .
+                            L.foldl1' R.append .
+                            L.map
+                              (R.extend (Z :. All :. All :. All :. (1 :: Int))) .
+                            L.map
+                              (\y ->
+                                  let weightArr =
+                                        listArray
+                                          ( (0, 0, 0)
+                                          , (ny' - 1, nx' - 1, nf' - 1)) .
+                                        R.toList $
+                                        weight' :: UArray (Int, Int, Int) Double
+                                      actArr = repaArray2UArray2 y
+                                  in uArray2RepaArray3 $
+                                     crossCorrelation25D stride actArr weightArr) $
+                            x)
+                        xs
+                        weightList
               else L.map
                      (\zs ->
                          let vec = fromListUnboxed (Z :. (L.length zs)) zs
