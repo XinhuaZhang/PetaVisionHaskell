cf275fd49bc88c45e47a938df48c0bfd95668e42
Xinhua Zhang <xinhua@unm.edu>
2017-04-14 11:15:06 -0600

    Added function that computes mean square error from a pvp file.

--- Application/PVPAnalysis/Analysis.hs
+++ Application/PVPAnalysis/Analysis.hs
@@ -1,30 +1,50 @@
 module Application.PVPAnalysis.Analysis where
 
-import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.IO.Class       (liftIO)
+import           Control.Monad.Trans.Resource
 import           Data.Conduit
-import           Data.Conduit.List      as CL
-import           Data.List              as L
-import           Data.Vector.Unboxed    as VU
+import           Data.Conduit.List            as CL
+import           Data.List                    as L
+import           Data.Vector.Unboxed          as VU
 import           PetaVision.PVPFile.IO
-import           Prelude                as P
+import           Prelude                      as P
 import           Text.Printf
 
 
-sparsity :: PVPHeader -> Sink PVPOutputData IO ()
-sparsity header =
-  do numActive <-
-       CL.fold (\b a ->
-                  case a of
-                    PVP_OUTPUT_ACT _ _ ->
-                      error "Cannot compute PVP_OUTPUT_ACT file's sparsity"
-                    PVP_OUTPUT_NONSPIKING_ACT _ x ->
-                      b + (VU.length . VU.filter (/= 0) $ x)
-                    PVP_OUTPUT_ACT_SPARSEVALUES _ x -> b + (VU.length x))
-               0
-     let percent = fromIntegral numActive / fromIntegral totalNumEle
-     liftIO $
-       printf "%0.1f%%(%d/%d Avg. Activatived)\n"
-              (percent * 100 :: Double)
-              (round $ percent * fromIntegral (nf header) :: Int)
-              (nf header)
-  where totalNumEle = (nBands header) * (nx header) * (ny header) * (nf header)
+sparsity :: PVPHeader -> Sink PVPOutputData (ResourceT IO) ()
+sparsity header = do
+  numActive <-
+    CL.fold
+      (\b a ->
+          case a of
+            PVP_OUTPUT_ACT _ _ ->
+              error "Cannot compute PVP_OUTPUT_ACT file's sparsity"
+            PVP_OUTPUT_NONSPIKING_ACT _ x ->
+              b + (VU.length . VU.filter (/= 0) $ x)
+            PVP_OUTPUT_ACT_SPARSEVALUES _ x -> b + VU.length x
+            _ -> error "sparsity: pvpFile type is not supported.")
+      0
+  let percent = fromIntegral numActive / fromIntegral totalNumEle
+  liftIO $
+    printf
+      "%0.1f%%(%d/%d Avg. Activatived)\n"
+      (percent * 100 :: Double)
+      (round $ percent * fromIntegral (nf header) :: Int)
+      (nf header)
+  where
+    totalNumEle = nBands header * nx header * ny header * nf header
+
+
+averageError :: PVPHeader -> Sink PVPOutputData (ResourceT IO) ()
+averageError header = do
+  errorSum <-
+    CL.fold
+      (\b a ->
+          case a of
+            PVP_OUTPUT_NONSPIKING_ACT _ x ->
+              b + sqrt (VU.sum . VU.map (^ (2 :: Int)) $ x)
+            _ -> error "sparsity: pvpFile type is not supported.")
+      0
+  let numEle = nx header * ny header * nf header
+      avgError = errorSum / fromIntegral numEle / fromIntegral (nBands header)
+  liftIO . print $ avgError
--- Application/PVPAnalysis/MainAvgError.hs
+++ Application/PVPAnalysis/MainAvgError.hs
@@ -3,10 +3,10 @@ module Main where
 import           Application.PVPAnalysis.Analysis
 import           Control.Monad.Trans.Resource
 import           Data.Conduit
-import           PetaVision.PVPFile.IO
+import PetaVision.PVPFile.IO
 import           System.Environment
 
 main = do
   (filePath:_) <- getArgs
   header <- readPVPHeader filePath
-  runResourceT $ pvpFileSource filePath $$ sparsity header
+  runResourceT $ pvpFileSource filePath $$ averageError header
