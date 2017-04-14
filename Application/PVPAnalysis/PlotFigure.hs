ac7b48c9e32912f7e4b04f55582929c13488e92b
xinhuazhang <fantasyfrog@gmail.com>
2017-04-14 11:28:22 -0600

    Added missing file

---
+++ Application/PVPAnalysis/PlotFigure.hs
@@ -0,0 +1,42 @@
+module Application.PVPAnalysis.PlotFigure where
+
+import           Control.Lens
+import           Data.Colour
+import           Data.Colour.Names
+import           Data.Default.Class
+import           Data.List                              as L
+import           Data.Vector.Unboxed                    as VU
+import           Graphics.Rendering.Chart
+import           Graphics.Rendering.Chart.Backend.Cairo
+import           Text.Printf
+
+plotSparsityNHistogram
+  :: FilePath -> (Double,VU.Vector Double) -> IO (PickFn ())
+plotSparsityNHistogram filePath (percent,vec) =
+  renderableToFile def filePath $ toRenderable layout
+  where nf' = VU.length vec
+        xs =
+          L.zipWith (\i v -> (i,[v]))
+                    [1 ..] .
+          L.reverse .
+          L.sort .
+          VU.toList 
+          -- VU.map (\x -> fromIntegral x / fromIntegral (VU.sum vec) :: Double) 
+          $
+          vec
+        title = printf "Sparsity: %0.1f%%(%d/%d Avg. Activatived)\n"
+                       (percent * 100 :: Double)
+                       (round $ percent * fromIntegral nf' :: Int)
+                       nf'
+        layout = layout_title .~ title
+               $ layout_title_style . font_size .~ 20
+               $ layout_left_axis_visibility . axis_show_ticks .~ False
+               $ layout_plots .~ [ plotBars bars ]
+               $ def :: Layout PlotIndex Double
+        bars = plot_bars_titles .~ [""]
+             $ plot_bars_values .~ xs
+             $ plot_bars_style .~ BarsClustered
+             $ plot_bars_spacing .~ BarsFixGap 0 5
+             $ plot_bars_item_styles .~ L.map mkStyle (cycle defaultColorSeq)
+             $ def
+        mkStyle c = (solidFillStyle c, Just (solidLine 1.0 $ opaque black))
